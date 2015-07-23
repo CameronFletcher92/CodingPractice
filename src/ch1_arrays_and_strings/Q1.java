7d45d0605fd5a863652d685cd92e39ae38397b30
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-07-23 11:47:34 +1000

    ch1 q1-q3

--- src/ch1_arrays_and_strings/Q1.java
+++ src/ch1_arrays_and_strings/Q1.java
@@ -2,13 +2,13 @@ package ch1_arrays_and_strings;
 
 public class Q1 {
     public static boolean hasUniqueChars(String str) {
-        // only 128 possible chars assuming ascii
-        if (str.length() > 128) {
+        // only 256 possible chars assuming ascii
+        if (str.length() > 256) {
             return false;
         }
 
         // array of booleans to detect duplicates
-        boolean[] charSet = new boolean[128];
+        boolean[] charSet = new boolean[256];
         for (char c : str.toCharArray()) {
             if (charSet[c]) {
                 return false;
---
+++ src/ch1_arrays_and_strings/Q3.java
@@ -0,0 +1,26 @@
+package ch1_arrays_and_strings;
+
+public class Q3 {
+    public static boolean isPermutation(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] counts = new int[256];
+
+        // populate the counts array
+        for (char c : str1.toCharArray()) {
+            counts[c]++;
+        }
+
+        // subtract as each character is hit and ensure it doesn't go below 0
+        for (char c : str2.toCharArray()) {
+            counts[c]--;
+            if (counts[c] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
