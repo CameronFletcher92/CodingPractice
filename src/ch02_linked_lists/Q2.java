72dcf2a517ee29a50d560c82078fc449c9ff8de9
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-07-28 15:02:38 +1000

    ch1 q2

--- src/ch02_linked_lists/Q2.java
+++ src/ch02_linked_lists/Q2.java
@@ -1,4 +1,22 @@
 package ch02_linked_lists;
 
 public class Q2 {
+    public static <E> E findKthLast(LinkedList<E> linkedList, int k) {
+        LinkedList.Node p1 = linkedList.head;
+        LinkedList.Node p2 = linkedList.head;
+
+        // move p2 up k elements
+        for (int i = 0; i < k; i++) {
+            p2 = p2.next;
+        }
+
+        // move p2 to the end of the list
+        while(p2 != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+
+        // p1 should now be at length - k
+        return (E)p1.data;
+    }
 }
--- src/ch02_linked_lists/Q2Test.java
+++ src/ch02_linked_lists/Q2Test.java
@@ -2,5 +2,14 @@ package ch02_linked_lists;
 import junit.framework.TestCase;
 
 public class Q2Test extends TestCase {
+    public void testFindKthLast() throws Exception {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
 
+        assertEquals(3, (int)Q2.findKthLast(list, 2));
+        assertEquals(1, (int)Q2.findKthLast(list, 4));
+    }
 }
