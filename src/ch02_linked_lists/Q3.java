1f1eddf32bc4bc3c0a7f01c6eb563bf2b8874d12
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-07-28 15:13:18 +1000

    ch2 q3

--- src/ch02_linked_lists/Q3.java
+++ src/ch02_linked_lists/Q3.java
@@ -1,4 +1,11 @@
 package ch02_linked_lists;
 
 public class Q3 {
+    public static void deleteNode(LinkedList.Node node) {
+        // copy the data from the next node to this node
+        node.data = node.next.data;
+
+        // remove the next node
+        node.next = node.next.next;
+    }
 }
--- src/ch02_linked_lists/Q3Test.java
+++ src/ch02_linked_lists/Q3Test.java
@@ -2,5 +2,23 @@ package ch02_linked_lists;
 import junit.framework.TestCase;
 
 public class Q3Test extends TestCase {
+    public void testDeleteNode() throws Exception {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
 
+        // get the '3' node
+        LinkedList.Node node = list.head.next.next;
+        Q3.deleteNode(node);
+
+        assertEquals(4, list.length());
+
+        assertEquals(1, (int)list.head.data);
+        assertEquals(2, (int)list.head.next.data);
+        assertEquals(4, (int)list.head.next.next.data);
+        assertEquals(5, (int)list.head.next.next.next.data);
+    }
 }
