25888584fea16a328b97b7b24ee01b750edb0b92
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-07-30 16:35:34 +1000

    ch3 q2

--- src/ch03_stacks_and_queues/Q2.java
+++ src/ch03_stacks_and_queues/Q2.java
@@ -1,4 +1,37 @@
 package ch03_stacks_and_queues;
 
 public class Q2 {
+    public static class MinStack extends Stack<Integer>{
+        private Stack<Integer> mins;
+
+        public MinStack() {
+            super();
+            mins = new Stack<>();
+        }
+
+        public Integer getMin() {
+            return mins.peek();
+        }
+
+        public void push(int value) {
+            super.push(value);
+            if (mins.peek() == null || value < mins.peek()) {
+                mins.push(value);
+            }
+        }
+
+        public Integer pop() {
+            Integer val = super.pop();
+            if (val == null) {
+                return null;
+            }
+
+            // pop the min off if it is this value
+            if (mins.peek() == val) {
+                mins.pop();
+            }
+
+            return val;
+        }
+    }
 }
--- src/ch03_stacks_and_queues/Q2Test.java
+++ src/ch03_stacks_and_queues/Q2Test.java
@@ -2,5 +2,24 @@ package ch03_stacks_and_queues;
 import junit.framework.TestCase;
 
 public class Q2Test extends TestCase {
+    public void testMinStack() throws Exception {
+        Q2.MinStack ms = new Q2.MinStack();
+
+        ms.push(5);
+        ms.push(4);
+        ms.push(3);
+        ms.push(2);
+        ms.push(1);
+
+        assertEquals(1, (int)ms.getMin());
+        ms.pop();
+        assertEquals(2, (int)ms.getMin());
+        ms.pop();
+        assertEquals(3, (int)ms.getMin());
+        ms.pop();
+        assertEquals(4, (int)ms.getMin());
+        ms.pop();
+        assertEquals(5, (int)ms.getMin());
+    }
 
 }
