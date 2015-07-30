f930c46ed70927a4abc44cbcc3ba7db39e6f45c5
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-07-30 17:12:36 +1000

    ch3 q3

--- src/ch03_stacks_and_queues/Q3.java
+++ src/ch03_stacks_and_queues/Q3.java
@@ -1,4 +1,61 @@
 package ch03_stacks_and_queues;
 
+import java.util.ArrayList;
+
 public class Q3 {
+    public static class SetOfStacks {
+        private ArrayList<Stack<Integer>> stacks;
+        private int capacity;
+        private int totalSize = 0;
+        private Stack<Integer> last;
+        private int lastSize;
+
+        public SetOfStacks(int capacity) {
+            this.capacity = capacity;
+            lastSize = 0;
+
+            stacks = new ArrayList<>();
+        }
+
+        public void push(Integer value) {
+            if (last != null && lastSize < capacity) {
+                last.push(value);
+                lastSize++;
+            } else {
+                last = new Stack<>();
+                stacks.add(last);
+                last.push(value);
+                lastSize = 1;
+            }
+        }
+
+        public Integer peek() {
+            if (stacks.size() == 0) {
+                return null;
+            }
+            return last.peek();
+        }
+
+        public Integer pop() {
+            if (stacks.size() == 0) {
+                return null;
+            }
+
+            int val = last.pop();
+            lastSize--;
+
+            if (lastSize == 0) {
+                stacks.remove(last);
+                if (stacks.size() > 0) {
+                    last = stacks.get(stacks.size() - 1);
+                    lastSize = capacity;
+                } else {
+                    last = null;
+                    lastSize = 0;
+                }
+            }
+
+            return val;
+        }
+    }
 }
--- src/ch03_stacks_and_queues/Q3Test.java
+++ src/ch03_stacks_and_queues/Q3Test.java
@@ -2,5 +2,37 @@ package ch03_stacks_and_queues;
 import junit.framework.TestCase;
 
 public class Q3Test extends TestCase {
+    public void testSetOfStacks() throws Exception {
+        Q3.SetOfStacks ss = new Q3.SetOfStacks(3);
+        ss.push(1);
+        ss.push(2);
+        ss.push(3);
+        ss.push(4);
+        ss.push(5);
+        ss.push(6);
+        ss.push(7);
+        ss.push(8);
+        ss.push(9);
+
+        assertEquals(9, (int)ss.pop());
+        assertEquals(8, (int)ss.pop());
+        assertEquals(7, (int)ss.pop());
+        assertEquals(6, (int)ss.pop());
+        assertEquals(5, (int)ss.pop());
+        assertEquals(4, (int)ss.pop());
+        assertEquals(3, (int)ss.pop());
+        assertEquals(2, (int)ss.pop());
+        assertEquals(1, (int)ss.pop());
+        assertEquals(null, ss.pop());
+
+        ss.push(1);
+        ss.push(2);
+        ss.push(3);
+
+        assertEquals(3, (int)ss.pop());
+        assertEquals(2, (int)ss.pop());
+        assertEquals(1, (int)ss.pop());
+        assertEquals(null, ss.pop());
+    }
 
 }
