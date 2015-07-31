1eaa973501fdf7f84b481b9cfdc4b691c4598461
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-07-31 12:46:53 +1000

    ch3 q6

--- src/ch03_stacks_and_queues/Q6.java
+++ src/ch03_stacks_and_queues/Q6.java
@@ -1,4 +1,22 @@
 package ch03_stacks_and_queues;
 
 public class Q6 {
+    public static Stack<Integer> sortStack(Stack<Integer> s1) {
+        Stack<Integer> s2 = new Stack<>();
+
+        while (s1.peek() != null) {
+            // get the top element of s1, and find where to place it in s2
+            int tmp = s1.pop();
+
+            // pop from s2 until we find the correct place for tmp
+            while (s2.peek() != null && s2.peek() > tmp) {
+                s1.push(s2.pop());
+            }
+
+            // push tmp onto s2 in the correct position
+            s2.push(tmp);
+        }
+
+        return s2;
+    }
 }
--- src/ch03_stacks_and_queues/Q6Test.java
+++ src/ch03_stacks_and_queues/Q6Test.java
@@ -2,5 +2,23 @@ package ch03_stacks_and_queues;
 import junit.framework.TestCase;
 
 public class Q6Test extends TestCase {
+    public void testSortStack() throws Exception {
+        Stack<Integer> s = new Stack<>();
 
+        s.push(5);
+        s.push(4);
+        s.push(6);
+        s.push(1);
+        s.push(3);
+        s.push(2);
+
+        s = Q6.sortStack(s);
+
+        assertEquals(6, (int)s.pop());
+        assertEquals(5, (int)s.pop());
+        assertEquals(4, (int)s.pop());
+        assertEquals(3, (int)s.pop());
+        assertEquals(2, (int)s.pop());
+        assertEquals(1, (int)s.pop());
+    }
 }
