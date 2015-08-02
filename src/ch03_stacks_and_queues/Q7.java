00da882c50281d54e74911b38fc6f74cbd47f256
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-02 12:44:38 +1000

    ch3 q7

--- src/ch03_stacks_and_queues/Q7.java
+++ src/ch03_stacks_and_queues/Q7.java
@@ -1,4 +1,66 @@
 package ch03_stacks_and_queues;
 
+import java.util.LinkedList;
+
 public class Q7 {
+    public static class Animal {
+        String name;
+        String type;
+        int added;
+
+        Animal(String n, String t) {
+            name = n;
+            type = t;
+        }
+    }
+
+    public static class AnimalShelter {
+        private LinkedList<Animal> dogList;
+        private LinkedList<Animal> catList;
+        private int timeStamp;
+
+        public AnimalShelter() {
+            dogList = new LinkedList<>();
+            catList = new LinkedList<>();
+        }
+
+        public void enqueue(Animal animal) {
+            animal.added = timeStamp++;
+
+            if (animal.type == "dog") {
+                dogList.addLast(animal);
+            } else if (animal.type == "cat") {
+                catList.addLast(animal);
+            }
+        }
+
+        public Animal dequeueDog() {
+            return dogList.isEmpty() ? null : dogList.removeFirst();
+        }
+
+        public Animal dequeueCat() {
+            return catList.isEmpty() ? null : catList.removeFirst();
+        }
+
+        public Animal dequeueAny() {
+            Integer oldestDog = dogList.isEmpty() ? null : dogList.getFirst().added;
+            Integer oldestCat = catList.isEmpty() ? null : catList.getFirst().added;
+
+            // deal with the null cases
+            if (oldestDog == null && oldestCat != null) {
+                return catList.removeFirst();
+            } else if (oldestCat == null && oldestDog != null) {
+                return dogList.removeFirst();
+            } else if (oldestCat == null && oldestDog == null) {
+                return null;
+            }
+
+            // return the older animal
+            if (oldestDog < oldestCat) {
+                return dogList.removeFirst();
+            } else {
+                return catList.removeFirst();
+            }
+        }
+    }
 }
--- src/ch03_stacks_and_queues/Q7Test.java
+++ src/ch03_stacks_and_queues/Q7Test.java
@@ -3,4 +3,30 @@ import junit.framework.TestCase;
 
 public class Q7Test extends TestCase {
 
+    public void testAnimalShelter() throws Exception {
+        Q7.AnimalShelter shelter = new Q7.AnimalShelter();
+
+        shelter.enqueue(new Q7.Animal("cat1", "cat"));
+        shelter.enqueue(new Q7.Animal("dog1", "dog"));
+        shelter.enqueue(new Q7.Animal("dog2", "dog"));
+        shelter.enqueue(new Q7.Animal("dog3", "dog"));
+        shelter.enqueue(new Q7.Animal("dog4", "dog"));
+        shelter.enqueue(new Q7.Animal("cat2", "cat"));
+        shelter.enqueue(new Q7.Animal("cat3", "cat"));
+
+        assertEquals("cat1", shelter.dequeueAny().name);
+        assertEquals("dog1", shelter.dequeueAny().name);
+        assertEquals("dog2", shelter.dequeueAny().name);
+        assertEquals("dog3", shelter.dequeueDog().name);
+        assertEquals("cat2", shelter.dequeueCat().name);
+        assertEquals("dog4", shelter.dequeueAny().name);
+        assertEquals("cat3", shelter.dequeueAny().name);
+
+        shelter.enqueue(new Q7.Animal("cat2", "cat"));
+        shelter.enqueue(new Q7.Animal("cat3", "cat"));
+
+        assertEquals("cat2", shelter.dequeueCat().name);
+        assertEquals("cat3", shelter.dequeueAny().name);
+    }
+
 }
