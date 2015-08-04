95884d5d2317fc834473720c0b025aa55f9c1995
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-04 15:01:30 +1000

    ch4 q3

--- src/ch04_trees_and_graphs/Q3.java
+++ src/ch04_trees_and_graphs/Q3.java
@@ -1,4 +1,21 @@
 package ch04_trees_and_graphs;
 
 public class Q3 {
+    private static TreeNode createMinimalBST(int[] nums, int start, int end) {
+        if (end < start) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.leftChild = createMinimalBST(nums, start, mid - 1);
+        root.rightChild = createMinimalBST(nums, mid + 1, end);
+
+        return root;
+    }
+
+    public static TreeNode createMinimalBST(int[] nums) {
+        return createMinimalBST(nums, 0, nums.length - 1);
+    }
+
 }
--- src/ch04_trees_and_graphs/Q3Test.java
+++ src/ch04_trees_and_graphs/Q3Test.java
@@ -3,4 +3,36 @@ import junit.framework.TestCase;
 
 public class Q3Test extends TestCase {
 
+    public void testCreateMinimalBST() {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        TreeNode root = Q3.createMinimalBST(nums);
+
+        assertEquals(root.data, 5);
+
+        assertEquals(root.leftChild.data, 2);
+        assertEquals(root.rightChild.data, 7);
+
+        assertEquals(root.leftChild.leftChild.data, 1);
+        assertEquals(root.leftChild.rightChild.data, 3);
+
+        assertEquals(root.rightChild.leftChild.data, 6);
+        assertEquals(root.rightChild.rightChild.data, 8);
+
+        assertEquals(root.leftChild.rightChild.rightChild.data, 4);
+        assertEquals(root.rightChild.rightChild.rightChild.data, 9);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6};
+
+        root = Q3.createMinimalBST(nums);
+
+        assertEquals(root.data, 3);
+        assertEquals(root.leftChild.data, 1);
+        assertEquals(root.leftChild.rightChild.data, 2);
+
+        assertEquals(root.rightChild.data, 5);
+        assertEquals(root.rightChild.leftChild.data, 4);
+        assertEquals(root.rightChild.rightChild.data, 6);
+    }
+
 }
