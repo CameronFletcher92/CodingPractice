13cc14590bb325486a2eb918f1feba167e0f9cdb
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-04 15:26:32 +1000

    ch4 q4

--- src/ch04_trees_and_graphs/Q4.java
+++ src/ch04_trees_and_graphs/Q4.java
@@ -1,4 +1,37 @@
 package ch04_trees_and_graphs;
 
+import java.util.LinkedList;
+
 public class Q4 {
+    private static void createLists(TreeNode node, int depth, LinkedList<LinkedList<TreeNode>> lists) {
+        // base case
+        if (node == null)
+            return;
+
+        // get the depth list
+        LinkedList<TreeNode> depthList;
+
+        // if there isn't a linked list for that depth yet, add one
+        if (lists.size() < (depth + 1)) {
+            LinkedList<TreeNode> newList = new LinkedList<>();
+            lists.add(depth, newList);
+            depthList = newList;
+        } else {
+            depthList = lists.get(depth);
+        }
+
+        // add ourselves to the correct depth list
+        depthList.add(node);
+
+        // recurse through children at depth + 1
+        createLists(node.leftChild, depth + 1, lists);
+        createLists(node.rightChild, depth + 1, lists);
+    }
+
+    public static LinkedList<LinkedList<TreeNode>> createLists(TreeNode root) {
+        LinkedList<LinkedList<TreeNode>> lists = new LinkedList<>();
+        createLists(root, 0, lists);
+
+        return lists;
+    }
 }
--- src/ch04_trees_and_graphs/Q4Test.java
+++ src/ch04_trees_and_graphs/Q4Test.java
@@ -1,6 +1,45 @@
 package ch04_trees_and_graphs;
 import junit.framework.TestCase;
 
+import java.util.LinkedList;
+
 public class Q4Test extends TestCase {
+    public void testCreateLists() {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+
+        // construct a balanced tree;
+        TreeNode root = new TreeNode(0);
+
+        // level 1
+        root.leftChild = t1;
+        root.rightChild = t2;
+
+        // level 2
+        root.leftChild.leftChild = t3;
+        root.leftChild.rightChild = t4;
+        root.rightChild.leftChild = t5;
+        root.rightChild.rightChild = t6;
+
+        LinkedList<LinkedList<TreeNode>> lists = Q4.createLists(root);
+
+        LinkedList<TreeNode> depth0 = lists.get(0);
+        assertTrue(depth0.contains(root));
+
+        LinkedList<TreeNode> depth1 = lists.get(1);
+        assertTrue(depth1.contains(t1));
+        assertTrue(depth1.contains(t2));
+
+        LinkedList<TreeNode> depth2 = lists.get(2);
+        assertTrue(depth2.contains(t3));
+        assertTrue(depth2.contains(t4));
+        assertTrue(depth2.contains(t5));
+        assertTrue(depth2.contains(t6));
+
+    }
 
 }
