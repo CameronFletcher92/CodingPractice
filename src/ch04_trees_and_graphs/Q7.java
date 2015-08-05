6b2c328db1d88a96a9e92d0b353d28ace3126291
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-05 13:02:05 +1000

    ch4 q7

--- src/ch04_trees_and_graphs/Q7.java
+++ src/ch04_trees_and_graphs/Q7.java
@@ -1,4 +1,35 @@
 package ch04_trees_and_graphs;
 
 public class Q7 {
+    private static boolean isDescendant(TreeNode root, TreeNode t) {
+        if (root == null)
+            return false;
+
+        if (root == t) {
+            return true;
+        }
+
+        return isDescendant(root.leftChild, t) || isDescendant(root.rightChild, t);
+    }
+
+    private static TreeNode findCommonAncestorHelper(TreeNode root, TreeNode t1, TreeNode t2) {
+        boolean isT1OnLeft = isDescendant(root.leftChild, t1);
+        boolean isT2OnLeft = isDescendant(root.leftChild, t2);
+
+        if (isT1OnLeft && isT2OnLeft) {
+            return findCommonAncestorHelper(root.leftChild, t1, t2);
+        } else if (!isT1OnLeft && !isT2OnLeft) {
+            return findCommonAncestorHelper(root.rightChild, t1, t2);
+        } else {
+            return root;
+        }
+    }
+
+    public static TreeNode findCommonAncestor(TreeNode root, TreeNode t1, TreeNode t2) {
+        if (!isDescendant(root, t1) || !isDescendant(root, t2)) {
+            return null;
+        } else {
+            return findCommonAncestorHelper(root, t1, t2);
+        }
+    }
 }
--- src/ch04_trees_and_graphs/Q7Test.java
+++ src/ch04_trees_and_graphs/Q7Test.java
@@ -2,5 +2,27 @@ package ch04_trees_and_graphs;
 import junit.framework.TestCase;
 
 public class Q7Test extends TestCase {
+    public void testFindCommonAncestor() throws Exception {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
 
+        t4.leftChild = t2;
+        t4.rightChild = t6;
+
+        t2.leftChild = t1;
+        t2.rightChild = t3;
+
+        t6.leftChild = t5;
+        t6.rightChild = t7;
+
+        assertEquals(t6, Q7.findCommonAncestor(t4, t5, t7));
+        assertEquals(t2, Q7.findCommonAncestor(t4, t1, t3));
+        assertEquals(t4, Q7.findCommonAncestor(t4, t5, t1));
+        assertEquals(t4, Q7.findCommonAncestor(t4, t2, t7));
+    }
 }
