cb73cbaa6ffe095cb89aaea4948195b41a1f0fe0
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-05 13:14:43 +1000

    ch4 q8

--- src/ch04_trees_and_graphs/Q8.java
+++ src/ch04_trees_and_graphs/Q8.java
@@ -1,4 +1,28 @@
 package ch04_trees_and_graphs;
 
 public class Q8 {
+    private static boolean treeMatch(TreeNode t1, TreeNode t2) {
+        // null cases
+        if (t1 == null && t2 == null) {
+            return true;
+        } else if (t1 != null && t2 == null) {
+            return false;
+        } else if (t1 == null && t2 != null) {
+            return false;
+        }
+
+        if (t1.data == t2.data) {
+            return treeMatch(t1.leftChild, t2.leftChild) && treeMatch(t1.rightChild, t2.rightChild);
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isSubTree(TreeNode big, TreeNode small) {
+        if (treeMatch(big, small)) {
+            return true;
+        } else {
+            return treeMatch(big.leftChild, small) || treeMatch(big.rightChild, small);
+        }
+    }
 }
--- src/ch04_trees_and_graphs/Q8Test.java
+++ src/ch04_trees_and_graphs/Q8Test.java
@@ -2,5 +2,36 @@ package ch04_trees_and_graphs;
 import junit.framework.TestCase;
 
 public class Q8Test extends TestCase {
+    public void testIsSubTree() throws Exception {
+        // big tree
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
 
+        t4.leftChild = t2;
+        t4.rightChild = t6;
+
+        t2.leftChild = t1;
+        t2.rightChild = t3;
+
+        t6.leftChild = t5;
+        t6.rightChild = t7;
+
+        // small tree
+        TreeNode t5copy = new TreeNode(5);
+        TreeNode t6copy = new TreeNode(6);
+        TreeNode t7copy = new TreeNode(7);
+
+        t6copy.leftChild = t5copy;
+        t6copy.rightChild = t7copy;
+
+        assertTrue(Q8.isSubTree(t4, t6copy));
+
+        t7copy.data = 9;
+        assertFalse(Q8.isSubTree(t4, t6copy));
+    }
 }
