9e4b99dd3aaeb19efa7c1d040907b065d72267a0
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-15 19:20:35 +1000

    ch9 q4

--- src/ch09_recursion_and_dynamic_programming/Q4.java
+++ src/ch09_recursion_and_dynamic_programming/Q4.java
@@ -1,4 +1,22 @@
 package ch09_recursion_and_dynamic_programming;
 
+
+import java.util.ArrayList;
+
 public class Q4 {
+    public static ArrayList<ArrayList<Integer>> getAllSets(ArrayList<Integer> set) {
+        // create the set of sets, initialize with just the empty set
+        ArrayList<ArrayList<Integer>> allSets = new ArrayList<>();
+        allSets.add(new ArrayList<Integer>());
+
+        for (int num : set) {
+            int startingSize = allSets.size();
+            for (int i = 0; i < startingSize; i++) {
+                ArrayList<Integer> newSet = new ArrayList<>(allSets.get(i));
+                newSet.add(num);
+                allSets.add(newSet);
+            }
+        }
+        return allSets;
+    }
 }
--- src/ch09_recursion_and_dynamic_programming/Q4Test.java
+++ src/ch09_recursion_and_dynamic_programming/Q4Test.java
@@ -1,6 +1,15 @@
 package ch09_recursion_and_dynamic_programming;
 import junit.framework.TestCase;
 
-public class Q4Test extends TestCase {
+import java.util.ArrayList;
 
+public class Q4Test extends TestCase {
+    public void testGetAllSets() throws Exception {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+        assertEquals(8, Q4.getAllSets(nums).size());
+    }
 }
