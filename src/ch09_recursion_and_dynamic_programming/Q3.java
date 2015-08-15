32917e92851d37c901d2eb5ac99afe35ca001c99
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-15 17:05:31 +1000

    ch9 q3

--- src/ch09_recursion_and_dynamic_programming/Q3.java
+++ src/ch09_recursion_and_dynamic_programming/Q3.java
@@ -2,31 +2,32 @@ package ch09_recursion_and_dynamic_programming;
 
 public class Q3 {
     private static int findMagicIndex(int[] array, int start, int end) {
-        // magic index not found
-        if (start > end || start < 0 || end >= array.length) {
+        // base case: no magic index found
+        if (start > end || start < 0 || end > (array.length - 1)) {
             return -1;
         }
 
+        // get the mid index and mid value
         int midIndex = (start + end) / 2;
         int midValue = array[midIndex];
 
-        // found the magic index
+        // base case: magic index found
         if (midValue == midIndex) {
             return midIndex;
         }
 
         // search left
-        int newEnd = Math.min(midIndex - 1, midValue);
-        int left = findMagicIndex(array, start, newEnd);
-        if (left >= 0) {
-            return left;
+        int newEnd = Math.min(midValue, midIndex - 1);
+        int leftVal = findMagicIndex(array, start, newEnd);
+        if (leftVal >= 0) {
+            return leftVal;
         }
 
         // search right
-        int newStart = Math.max(midIndex + 1, midValue);
-        int right = findMagicIndex(array, newStart, end);
-        return right;
+        int newStart = Math.max(midValue, midIndex + 1);
+        return findMagicIndex(array, newStart, end);
     }
+
 
     public static int findMagicIndex(int[] array) {
         // use a binary search, starting at half way point
--- src/ch09_recursion_and_dynamic_programming/Q3Test.java
+++ src/ch09_recursion_and_dynamic_programming/Q3Test.java
@@ -3,7 +3,10 @@ import junit.framework.TestCase;
 
 public class Q3Test extends TestCase {
     public void testFindMagicIndex() throws Exception {
-        int[] array = new int[] {-5, -4, -3, -2, -1, 5, 10};
+        int[] array = new int[] {1, 2, 5, 5, 5, 5, 7, 9, 11 };
+        assertEquals(5, Q3.findMagicIndex(array));
+
+        array = new int[] {-5, -4, -3, -2, -1, 5, 10};
         assertEquals(5, Q3.findMagicIndex(array));
 
         array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -12,8 +15,6 @@ public class Q3Test extends TestCase {
         array = new int[] {0, 5, 10, 15, 20, 25, 30, 35, 40, 45};
         assertEquals(0, Q3.findMagicIndex(array));
 
-        array = new int[] {1, 2, 5, 5, 5, 5, 7, 9, 11 };
-        assertEquals(5, Q3.findMagicIndex(array));
     }
 
 }
