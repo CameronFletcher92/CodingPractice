602a7b45daf77f430a8363fdfeea96deaf75faa4
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-15 16:37:17 +1000

    ch4 Q3

--- src/ch09_recursion_and_dynamic_programming/Q3.java
+++ src/ch09_recursion_and_dynamic_programming/Q3.java
@@ -1,4 +1,37 @@
 package ch09_recursion_and_dynamic_programming;
 
 public class Q3 {
+    private static int findMagicIndex(int[] array, int start, int end) {
+        // magic index not found
+        if (start > end || start < 0 || end >= array.length) {
+            return -1;
+        }
+
+        int midIndex = (start + end) / 2;
+        int midValue = array[midIndex];
+
+        // found the magic index
+        if (midValue == midIndex) {
+            return midIndex;
+        }
+
+        // search left
+        int newEnd = Math.min(midIndex - 1, midValue);
+        int left = findMagicIndex(array, start, newEnd);
+        if (left >= 0) {
+            return left;
+        }
+
+        // search right
+        int newStart = Math.max(midIndex + 1, midValue);
+        int right = findMagicIndex(array, newStart, end);
+        return right;
+    }
+
+    public static int findMagicIndex(int[] array) {
+        // use a binary search, starting at half way point
+        return findMagicIndex(array, 0, array.length - 1);
+    }
+
+
 }
--- src/ch09_recursion_and_dynamic_programming/Q3Test.java
+++ src/ch09_recursion_and_dynamic_programming/Q3Test.java
@@ -2,5 +2,18 @@ package ch09_recursion_and_dynamic_programming;
 import junit.framework.TestCase;
 
 public class Q3Test extends TestCase {
+    public void testFindMagicIndex() throws Exception {
+        int[] array = new int[] {-5, -4, -3, -2, -1, 5, 10};
+        assertEquals(5, Q3.findMagicIndex(array));
+
+        array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertEquals(-1, Q3.findMagicIndex(array));
+
+        array = new int[] {0, 5, 10, 15, 20, 25, 30, 35, 40, 45};
+        assertEquals(0, Q3.findMagicIndex(array));
+
+        array = new int[] {1, 2, 5, 5, 5, 5, 7, 9, 11 };
+        assertEquals(5, Q3.findMagicIndex(array));
+    }
 
 }
