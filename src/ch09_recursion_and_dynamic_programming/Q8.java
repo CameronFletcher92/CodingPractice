a820608a5de4959e800e7cc2b7c967a696cd7a9f
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-17 15:22:11 +1000

    ch9 q8

--- src/ch09_recursion_and_dynamic_programming/Q8.java
+++ src/ch09_recursion_and_dynamic_programming/Q8.java
@@ -1,31 +1,52 @@
 package ch09_recursion_and_dynamic_programming;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Q8 {
     public static int makeChange(int n) {
         int[] denoms = {25, 10, 5, 1};
-        int[][] map = new int[n + 1][denoms.length];
+        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
         return makeChange(n, denoms, 0, map);
     }
 
-    private static int makeChange(int amount, int[] denoms, int index, int[][] map) {
-        if (map[amount][index] > 0) {
-            return map[amount][index];
+    public static void safeMapAdd(HashMap<Integer, HashMap<Integer, Integer>> map, int amount, int denom, int count) {
+        // create sub map
+        HashMap<Integer, Integer> subMap;
+        if (!map.containsKey(amount)) {
+            subMap = new HashMap<>();
+            map.put(amount, subMap);
+        } else {
+            subMap = map.get(amount);
         }
 
-        if (index >= denoms.length - 1) {
+        // create entry in the sub map
+        if (!subMap.containsKey(denom)) {
+            subMap.put(denom, count);
+        }
+    }
+
+    private static int makeChange(int amount, int[] denoms, int index, HashMap<Integer, HashMap<Integer, Integer>> map) {
+        // base case, last 1 cent
+        if (index == (denoms.length - 1)) {
             return 1;
         }
 
-        int denomAmount = denoms[index];
-        int ways = 0;
-        for (int i = 0; i * denomAmount <= amount; i++) {
-            int amountRemaining = amount - (i * denomAmount);
-            ways += makeChange(amountRemaining, denoms, index + 1, map);
+        int denom = denoms[index];
+
+        // retrieve cached value
+        if (map.containsKey(amount) && map.get(amount).containsKey(denom)) {
+            return map.get(amount).get(denom);
         }
 
-        map[amount][index] = ways;
-        return ways;
+        // count the number of ways
+        int count = 0;
+        for (int i = 0; (denom * i) <= amount; i++) {
+            int subAmount = amount - (denom * i);
+            count += makeChange(subAmount, denoms, index + 1, map);
+        }
+
+        // add to the hashmap
+        safeMapAdd(map, amount, denom, count);
+        return count;
     }
 }
