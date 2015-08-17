7cc1d91510045b1ad903eb1ea7d948f1c06a68f8
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-17 15:04:48 +1000

    ch9 q8

--- src/ch09_recursion_and_dynamic_programming/Q8Test.java
+++ src/ch09_recursion_and_dynamic_programming/Q8Test.java
@@ -2,5 +2,10 @@ package ch09_recursion_and_dynamic_programming;
 import junit.framework.TestCase;
 
 public class Q8Test extends TestCase {
-
+    public void testCountWays() throws Exception {
+        assertEquals(2, Q8.makeChange(5));
+        assertEquals(4, Q8.makeChange(10));
+        assertEquals(13, Q8.makeChange(25));
+        assertEquals(24, Q8.makeChange(35));
+    }
 }
