cc763db524a364da88fe03a521de811b8e1807b3
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-17 14:00:43 +1000

    ch9 q7

--- src/ch09_recursion_and_dynamic_programming/Q7.java
+++ src/ch09_recursion_and_dynamic_programming/Q7.java
@@ -1,4 +1,32 @@
 package ch09_recursion_and_dynamic_programming;
 
 public class Q7 {
+    public static enum Color {
+        Red, Blue, Green
+    }
+
+    private static void fill(Color[][] screen, int x, int y, Color oldColor, Color newColor) {
+        // base case, this pixel doesn't match the old color
+        if (screen[x][y] != oldColor || screen[x][y] == newColor || x < 0 || x > screen.length || y < 0 || y > screen[0].length) {
+            return;
+        }
+
+        // set the pixel
+        screen[x][y] = newColor;
+
+        // spread outward
+        fill(screen, x + 1, y, oldColor, newColor);
+        fill(screen, x - 1, y, oldColor, newColor);
+        fill(screen, x, y + 1, oldColor, newColor);
+        fill(screen, x, y - 1, oldColor, newColor);
+    }
+
+    public static void fill(Color[][] screen, int x, int y, Color newColor) {
+        // get the target pixel's color
+        Color oldColor = screen[x][y];
+        if (oldColor != newColor) {
+            fill(screen, x, y, oldColor, newColor);
+        }
+    }
+
 }
--- src/ch09_recursion_and_dynamic_programming/Q7Test.java
+++ src/ch09_recursion_and_dynamic_programming/Q7Test.java
@@ -1,6 +1,28 @@
 package ch09_recursion_and_dynamic_programming;
 import junit.framework.TestCase;
 
-public class Q7Test extends TestCase {
+import java.util.Arrays;
 
+public class Q7Test extends TestCase {
+    public void testFill() throws Exception {
+        Q7.Color[][] screen = new Q7.Color[][] {
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Blue, Q7.Color.Blue, Q7.Color.Blue, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Red, Q7.Color.Blue, Q7.Color.Blue, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Blue, Q7.Color.Blue, Q7.Color.Blue, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red }
+        };
+
+        Q7.fill(screen, 1, 1, Q7.Color.Green);
+
+        Q7.Color[][] expected = new Q7.Color[][] {
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Green, Q7.Color.Green, Q7.Color.Green, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Red, Q7.Color.Green, Q7.Color.Green, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Green, Q7.Color.Green, Q7.Color.Green, Q7.Color.Red },
+                new Q7.Color[] { Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red, Q7.Color.Red }
+        };
+
+        assertTrue(Arrays.deepEquals(expected, screen));
+    }
 }
