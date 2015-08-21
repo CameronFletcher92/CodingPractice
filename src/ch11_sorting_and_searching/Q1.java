f81e6a53d0b4036bdae023247bb372febd97b7fd
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-21 13:25:49 +1000

    ch11 q1

--- src/ch11_sorting_and_searching/Q1.java
+++ src/ch11_sorting_and_searching/Q1.java
@@ -1,4 +1,24 @@
 package ch11_sorting_and_searching;
 
 public class Q1 {
+    public static int[] mergeInto(int[] a, int aSize, int[] b) {
+        int aCursor = aSize - 1;
+        int bCursor = b.length - 1;
+
+        int mergedCursor = (aSize - 1) + b.length;
+
+        while (aCursor >= 0 || bCursor >= 0) {
+            if (bCursor == -1 || a[aCursor] > b[bCursor]) {
+                a[mergedCursor] = a[aCursor];
+                aCursor--;
+            } else {
+                a[mergedCursor] = b[bCursor];
+                bCursor--;
+            }
+            mergedCursor--;
+        }
+
+        return a;
+    }
+
 }
--- src/ch11_sorting_and_searching/Q1Test.java
+++ src/ch11_sorting_and_searching/Q1Test.java
@@ -2,5 +2,23 @@ package ch11_sorting_and_searching;
 import junit.framework.TestCase;
 
 public class Q1Test extends TestCase {
+    public void testMergeInto() throws Exception {
+        int[] a1 = new int[]{1, 3, 5, 7, 9, 0, 0, 0, 0, 0};
+        int[] a2 = new int[]{2, 4, 6, 8, 10};
+
+        int[] merged = Q1.mergeInto(a1, 5, a2);
+        for (int i = 0; i < 10; i++) {
+            assertEquals(i + 1, merged[i]);
+        }
+
+        a1 = new int[]{1, 3, 5, 7, 10, 11, 12, 0, 0, 0};
+        a2 = new int[]{1, 1, 1};
+
+        merged = Q1.mergeInto(a1, 7, a2);
+        for (int i = 0; i < 10; i++) {
+            assertEquals(i + 1, merged[i]);
+        }
+
+    }
 
 }
