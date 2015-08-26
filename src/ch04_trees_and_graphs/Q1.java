7944b9d3c2c49f57e499cf4cc464888868d71c6a
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-26 14:36:27 +1000

    ch4 q1 reimplemented

--- src/ch04_trees_and_graphs/Q1.java
+++ src/ch04_trees_and_graphs/Q1.java
@@ -1,23 +1,18 @@
 package ch04_trees_and_graphs;
 
 public class Q1 {
-    private static int checkHeight(TreeNode root) {
-        if (root == null) {
-            return 0;
+    private static int checkHeight(TreeNode n) {
+        // base case: leaf node
+        if (n.leftChild == null && n.rightChild == null) {
+            return 1;
         }
 
-        // get the subtree heights
-        int leftHeight = checkHeight(root.leftChild);
-        int rightHeight = checkHeight(root.rightChild);
+        // get the heights of the left and right subtrees
+        int leftHeight = n.leftChild != null ? checkHeight(n.leftChild) : 0;
+        int rightHeight = n.rightChild != null ? checkHeight(n.rightChild) : 0;
 
-        // if either subtree is unbalanced, this subtree is unbalanced
-        if (leftHeight == -1 || rightHeight == -1) {
-            return -1;
-        }
-
-        // check if this subtree is unbalanced, otherwise return the subtree's height
-        int heightDiff = Math.abs(leftHeight - rightHeight);
-        if (heightDiff > 1) {
+        // check subtrees were balanced, and that the 2 subtrees height difference does not exceed 1
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
             return -1;
         } else {
             return Math.max(leftHeight, rightHeight) + 1;
