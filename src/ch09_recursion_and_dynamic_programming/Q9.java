22ef4245106a5eacbc9581b9208c95ecb775a06b
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-08-27 16:56:50 +1000

    ch9 q9 reimplemented

--- src/ch09_recursion_and_dynamic_programming/Q9.java
+++ src/ch09_recursion_and_dynamic_programming/Q9.java
@@ -1,34 +1,35 @@
 package ch09_recursion_and_dynamic_programming;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Q9 {
-    // my implementation
-    public static class Location {
-        public int x;
-        public int y;
+    public static ArrayList<ArrayList<Point>> placeQueens() {
+        ArrayList<Point> queens = new ArrayList<>();
+        ArrayList<ArrayList<Point>> results = new ArrayList<>();
+        placeQueens(queens, 0, results);
 
-        public Location (int x, int y) {
-            this.x = x;
-            this.y = y;
+        return results;
+    }
+
+    private static void placeQueens(ArrayList<Point> queens, int y, ArrayList<ArrayList<Point>> ways) {
+        if (y == 8) {
+            ways.add((ArrayList<Point>)queens.clone());
         }
 
-        public String toString() {
-            return "(" + x + "," + y + ")";
+        for (int x = 0; x < 8; x++) {
+            Point p = new Point(x, y);
+            if (isValid(queens, p)) {
+                queens.add(p);
+                placeQueens(queens, y + 1, ways);
+                queens.remove(y);
+            }
         }
     }
 
-    private static boolean canPlace(Location newLoc, ArrayList<Location> queens) {
-        for (Location l : queens) {
-            // check if in same row / col
-            if (l.x == newLoc.x || l.y == newLoc.y) {
-                return false;
-            }
-
-            // check if on the same diagonal
-            int rowDist = Math.abs(newLoc.x - l.x);
-            int colDist = Math.abs(newLoc.y - l.y);
-            if (rowDist == colDist) {
+    private static boolean isValid(ArrayList<Point> queens, Point q) {
+        for (Point p : queens) {
+            if (p.x == q.x || p.y == q.y || Math.abs(p.x - q.x) == Math.abs(p.y - q.y)) {
                 return false;
             }
         }
@@ -36,29 +37,5 @@ public class Q9 {
         return true;
     }
 
-    private static void placeQueens(int row, ArrayList<Location> queens, ArrayList<ArrayList<Location>> results) {
-        // base case: we have placed all the queens
-        if (queens.size() == 8) {
-            results.add(queens);
-            return;
-        }
-
-        for (int col = 0; col < 8; col++) {
-            Location newLoc = new Location(row, col);
-            if (canPlace(newLoc, queens)) {
-                ArrayList<Location> newQueens = (ArrayList<Location>)queens.clone();
-                newQueens.add(newLoc);
-                placeQueens(row + 1, newQueens, results);
-            }
-        }
-    }
-
-    public static ArrayList<ArrayList<Location>> placeQueens() {
-        ArrayList<ArrayList<Location>> results = new ArrayList<>();
-        ArrayList<Location> queens = new ArrayList<>();
-
-        placeQueens(0, queens, results);
-        return results;
-    }
 
 }
